e5db729a1aab6d660de72ea331db0f64d433cb3c
t2othick <t2othick@gmail.com>
2018-04-28 12:02:03 +0800

    complete 16

--- 16.py
+++ 16.py
@@ -19,12 +19,41 @@
 
 
 class Solution(object):
+    def is_closest(self, target, current, next):
+        if abs(target - current) < abs(target - next):
+            return False
+        return True
+
+    def twoSum(self, nums, first, start, end, target):
+        closest = nums[first] + nums[start] + nums[end]
+        while start < end:
+            i_closest = nums[first] + nums[start] + nums[end]
+            # print 'test', target, i_closest, nums[first] + nums[start] + nums[end-1]
+            while start < end - 1 and self.is_closest(target, i_closest, nums[first] + nums[start] + nums[end-1]):
+                i_closest = nums[first] + nums[start] + nums[end-1]
+                end -= 1
+            if self.is_closest(target, closest, i_closest):
+                closest = i_closest
+            start += 1
+        return closest
+
+
     def threeSumClosest(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
+        nums = sorted(nums)
+        # print nums
+        closest = nums[0] + nums[1] + nums[2]
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            i_closest = self.twoSum(nums, i, i + 1, len(nums) - 1, target)
+            if self.is_closest(target, closest, i_closest):
+                closest = i_closest
+        return closest
 
 
-print Solution().threeSumClosest([-1, 0, 1, 2, -1, -4], 2)
+print Solution().threeSumClosest([6,-18,-20,-7,-15,9,18,10,1,-20,-17,-19,-3,-5,-19,10,6,-11,1,-17,-15,6,17,-18,-3,16,19,-20,-3,-17,-15,-3,12,1,-9,4,1,12,-2,14,4,-4,19,-20,6,0,-19,18,14,1,-15,-5,14,12,-4,0,-10,6,6,-6,20,-8,-6,5,0,3,10,7,-2,17,20,12,19,-13,-1,10,-1,14,0,7,-3,10,14,14,11,0,-4,-15,-8,3,2,-5,9,10,16,-4,-3,-9,-8,-14,10,6,2,-12,-7,-16,-6,10], -52)
